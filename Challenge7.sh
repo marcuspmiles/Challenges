e25c29902cb08c3f5db8c6898937eaf90b67cd7b
Marcus <marcuspmiles@gmail.com>
2021-08-03 13:50:18 -0800

    Challenge7

---
+++ Challenge7.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+#System Info Lookup
+echo System Info Lookup 
+    echo %%%%%%%%%%RAM INFO%%%%%%%%%%
+    lshw -class memory | grep -E 'description:|size'
+    echo %%%%%%%%%%CPU INFO%%%%%%%%%%
+    lshw -class cpu | grep -E 'product:|vendor'
+    echo %%%%%%%%%%DISPLAY ADAPTER%%%%%%%%%%
+    lshw -class display | grep -E 'description:|product|vendor|width'
+    echo %%%%%%%%%%NETWORK ADAPTER%%%%%%%%%%
+    lshw -class network | grep -E 'description:|product|vendor|version|size|capacity|serial'
+
+  
+
+
+
+
+
+
