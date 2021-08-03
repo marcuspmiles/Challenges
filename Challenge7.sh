7226bd49b07ced593c69606b17eda053663c0c86
Marcus Miles <marcuspmiles@gmail.com>
2021-08-03 14:52:31 -0700

    Update Challenge7.sh

--- Challenge7.sh
+++ Challenge7.sh
@@ -1,13 +1,13 @@
 #!bin/bash
 #System Info Lookup
 echo System Info Lookup 
-    echo %%%%%%%%%%RAM INFO%%%%%%%%%%
+    echo %%%%%%%%%% RAM INFO %%%%%%%%%%
     lshw -class memory | grep -E 'description:|size'
-    echo %%%%%%%%%%CPU INFO%%%%%%%%%%
+    echo %%%%%%%%%% CPU INFO %%%%%%%%%%
     lshw -class cpu | grep -E 'product:|vendor'
-    echo %%%%%%%%%%DISPLAY ADAPTER%%%%%%%%%%
+    echo %%%%%%%%%% DISPLAY ADAPTER %%%%%%%%%%
     lshw -class display | grep -E 'description:|product|vendor|width'
-    echo %%%%%%%%%%NETWORK ADAPTER%%%%%%%%%%
+    echo %%%%%%%%%% NETWORK ADAPTER %%%%%%%%%%
     lshw -class network | grep -E 'description:|product|vendor|version|size|capacity|serial'
 
   
