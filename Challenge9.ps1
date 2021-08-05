042dc81dceb3a103c7cde32bfc31cb0cb4191e98
Marcus Miles <marcuspmiles@gmail.com>
2021-08-05 15:33:15 -0700

    Update Challenge9.ps1

--- Challenge9.ps1
+++ Challenge9.ps1
@@ -3,10 +3,10 @@ Write-Host "My PowerShell Challenge 9 Script"
 $now = Get-Date
 $startdate = $now.AddDays(1)
 #Grabs all system logs within 24 hours and exports them to a text file
-$el = Get-EventLog -ComputerName -LogName System -After $startdate
+$el = Get-EventLog -LogName System -After $startdate
 $el | Export-Csv last_24.txt
 #Grabls all system error logs and exports them to a text file
-$e2 = Get-EventLog -ComputerName -LogName System -EntryType Error
+$e2 = Get-EventLog -LogName System -EntryType Error
 $e2 | Export-Csv errors.txt
 #Prints all system logs with the ID of 16
 Get-EventLog -LogName System -InstanceId 16 
