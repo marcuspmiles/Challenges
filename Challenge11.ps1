c9bb21aadaffe31e65e38a3e99679e1599179a6b
Marcus Miles <marcuspmiles@gmail.com>
2021-08-09 14:59:41 -0700

    Create Challenge11.ps1

---
+++ Challenge11.ps1
@@ -0,0 +1,13 @@
+#Enables file & printer sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+#Allows inbound ICMP traffic
+New-NetFirewallRule -DisplayName "Allow inbound ICMPv4" -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -RemoteAddress localsubnet -Action Allow
+New-NetFirewallRule -DisplayName "Allow inbound ICMPv6" -Direction Inbound -Protocol ICMPv6 -IcmpType 8 -RemoteAddress localsubnet -Action Allow
+#Enables Remoting
+Enable-PSRemoting
+#Removes Bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+#Enables HyperV
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+#Tobbles SMB1 protocol to false, disabling it
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
